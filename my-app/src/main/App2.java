916a08f146098d7cd759dfa79257db0dbbeed39f
srsalari <sr_salari@hotmail.com>
2024-12-19 22:42:48 -0500

    This a Maven project in DevOps course

---
+++ my-app/src/main/App2.java
@@ -0,0 +1,10 @@
+package com.mycompany.app2;
+
+/**
+ * Hello world!
+ */
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("Hello from AWS DevOps!");
+    }
+}
